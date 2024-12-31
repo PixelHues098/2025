0901776a4f4caa0198c1a94dec6bfa537c2a5e3e
PixelHues098 <mobiemocken0978@gmail.com>
2024-12-31 16:26:30 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let letters = document.querySelectorAll('h2 span');
+letters.forEach(e => {
+    e.style.color = `rgb(${Math.floor(255 * Math.random())}, ${Math.floor(255 * Math.random())}, ${Math.floor(255 * Math.random())})`;
+})
+setTimeout(() => {
+    $('section').fireworks();
+}, 10000)
